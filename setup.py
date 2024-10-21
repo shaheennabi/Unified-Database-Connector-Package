899bbd490d129aaf11c8ce09b61d136104e55ab9
shaheennabi <ishaheennabi333@gmail.com>
2024-10-21 05:44:19 -0700

    updated

--- setup.py
+++ setup.py
@@ -1,30 +1,47 @@
 from setuptools import setup, find_packages
 from typing import List
+import os
 
-with open('README.md', 'r', encoding='utf-8') as f:
-    long_description = f.read()     
-   
+HYPHEN_E_DOT = '-e .'
 
-__version__ = "0.0.4"
+def get_requirements(file_path: str) -> List[str]:
+    """Reads the requirements file and returns a list of dependencies."""
+    requirements = []
+    if os.path.exists(file_path):
+        with open(file_path, encoding="utf-8") as f:
+            requirements = f.readlines()
+            requirements = [req.strip() for req in requirements]
+            if HYPHEN_E_DOT in requirements:
+                requirements.remove(HYPHEN_E_DOT)
+    else:
+        raise FileNotFoundError(f"{file_path} does not exist.")
+    return requirements
+
+
+__version__ = "0.0.1"
 REPO_NAME = "Unified-Database-Connector-Package"
-PKG_NAME= "Unified-Database-Connector-Package"
+PKG_NAME = "Unified-Database-Connector"
 AUTHOR_USER_NAME = "shaheennabi"
 AUTHOR_EMAIL = "ishaheennabi333@gmail.com"
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name=PKG_NAME,
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
-    description="A python package for connecting with database.",
+    description="A Python package for connecting with databases.",
     long_description=long_description,
-    long_description_content="text/markdown",
+    long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    )
-
-
+    install_requires=get_requirements("./requirements_dev.txt"),
+    include_package_data=True,
+    zip_safe=False,
+)
