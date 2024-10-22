41bd00f1607b5cb35c32437871d00b315e402293
shaheennabi <ishaheennabi333@gmail.com>
2024-10-22 03:39:46 -0700

    updated

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ except FileNotFoundError:
     long_description = "A Python package for connecting with databases."
 
 setup(
-    name="Database Automator",
+    name="database automator",
     version="0.0.3",
     author="shaheennabi",
     author_email="ishaheennabi333@gmail.com",
